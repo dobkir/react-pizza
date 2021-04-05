d420e6d44e5d48c827532fc4dce437bf8f760cb6
dobkir <dobkir@gmail.com>
2021-04-05 21:17:12 +0400

    5 step. Redux. Store creation. Action creators and reducers for filters, pizzas. Using connect()() in App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import React from 'react';
 import axios from 'axios';
+import { connect } from 'react-redux';
 import { Route } from 'react-router';
 import { Header } from './components';
 import { Home, Cart } from './pages';
+import { setPizzas } from './redux/actions/pizzas';
 
-
-function App() {
-  const [pizzas, setPizzas] = React.useState([]);
-
-  React.useEffect(() => {
+class App extends React.Component {
+  componentDidMount() {
     axios.get('http://localhost:3000/db.json').then(({ data }) => {
-      setPizzas(data.pizzas);
+      this.props.setPizzas(data.pizzas);
     });
-  }, []);
-
-  return (
-    <div className='wrapper'>
-      <Header />
-      <div className='content'>
-        <Route path='/' render={() => <Home items={pizzas} />} exact />
-        <Route path='/cart' component={Cart} exact />
+  };
+  render() {
+    console.log(this.props);
+    return (
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <Route path="/" render={() => <Home items={this.props.items} />} exact />
+          <Route path="/cart" component={Cart} exact />
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  };
+};
 
-export default App;
+export default connect(
+  (state) => {
+    return {
+      items: state.pizzas.items,
+      filters: state.filters,
+    };
+  },
+  (dispatch) => {
+    return {
+      setPizzas: (items) => dispatch(setPizzas(items)),
+    };
+  },
+)(App);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,22 @@ import { Categories, PizzaBlock, SortPopup } from '../components';
 
 function Home({ items }) {
   return (
-    <div className="container">
-      <div className="content__top">
+    <div className='container'>
+      <div className='content__top'>
         <Categories
           onClickItem={(name) => console.log(name)}
           items={['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']}
         />
-        <SortPopup items={['популярности', 'цене', 'алфавит']} />
+        <SortPopup
+          items={[
+            { name: 'популярности', type: 'popular' },
+            { name: 'цене', type: 'price' },
+            { name: 'алфавит', type: 'alphabet' },
+          ]}
+        />
       </div>
-      <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
+      <h2 className='content__title'>Все пиццы</h2>
+      <div className='content__items'>
         {items.map((obj) => (
           <PizzaBlock key={obj.id} {...obj} />
         ))}
